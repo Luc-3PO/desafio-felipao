8355c2b9c488215e1c5f8a1c1eecf3dd6b879e53
Lucas Rosa <123273491+Luc-3PO@users.noreply.github.com>
2024-01-17 23:45:32 +0000

    adicionando laço de repetição

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 var nomeHeroi = "Boromir"
 var nivelHeroi = 8.000
+var nomeNivel = "" // Variável que armazena o nível
 
-if (nivelHeroi === 1.000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Ferro")
-} else if (nivelHeroi >= 1.001 && nivelHeroi <= 2.000) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Bronze")
-} else if (nivelHeroi >= 2.001 && nivelHeroi <= 5.000) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Prata")
-} else if (nivelHeroi >= 5.001 && nivelHeroi <= 7.000) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Ouro")
-} else if (nivelHeroi >= 7.001 && nivelHeroi <= 8.000) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Platina")
-} else if (nivelHeroi >= 8.001 && nivelHeroi <= 9.000) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Ascendente")
-} else if (nivelHeroi >= 9.001 && nivelHeroi <= 10.000) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Imortal")
-} else if (nivelHeroi > 10.001) {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante")
-} else {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível indefinido")
+while (nivelHeroi > 0) {
+    if (nivelHeroi === 1.000) {
+        nomeNivel = "Ferro"
+        break
+    } else if (nivelHeroi >= 1.001 && nivelHeroi <= 2.000) {
+        nomeNivel = "Bronze"
+        break
+    } else if (nivelHeroi >= 2.001 && nivelHeroi <= 5.000) {
+        nomeNivel = "Prata"
+        break
+    } else if (nivelHeroi >= 5.001 && nivelHeroi <= 7.000) {
+        nomeNivel = "Ouro"
+        break
+    } else if (nivelHeroi >= 7.001 && nivelHeroi <= 8.000) {
+        nomeNivel = "Platina"
+        break
+    } else if (nivelHeroi >= 8.001 && nivelHeroi <= 9.000) {
+        nomeNivel = "Ascendente"
+        break
+    } else if (nivelHeroi >= 9.001 && nivelHeroi <= 10.000) {
+        nomeNivel = "Imortal"
+        break
+    } else if (nivelHeroi > 10.001) {
+        nomeNivel = "Radiante"
+        break
+    } else {
+        nomeNivel = "Indefinido"
+        break
+    }
 }
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível " + nomeNivel)
